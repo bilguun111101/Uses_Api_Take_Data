6a4b45197d47f1d9125bee8370ac7af0a946b066
bilguun70b1123@gmail.com <bilguun70b1123@gmail.com>
2022-08-26 10:54:12 +0800

    Take data from API

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Cart from './components/Carts/Cart';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Cart />
+    </div>
+  );
+}
+
+export default App;
